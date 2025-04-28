494710138eb2c5172744330727e0a8403100c5a2
Yuvraj Gupta <64892401+yuvrajgupta1@users.noreply.github.com>
2025-04-28 14:52:21 -0700

    Dashboard Layout & UI, Google Auth

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,17 +1,57 @@
-import { Header } from '../../../components'
+import { Header, StatsCard, TripCard } from 'components'
 import React from 'react'
+import { dashboardStats, user, allTrips } from '~/constants';
+
+const {totalUsers, usersJoined, totalTrips, tripsCreated, userRole} = dashboardStats;
 
 const Dashboard = () => {
-
-  const user = {name: 'John Doe'}
 
   return (
     <main className='dashboard wrapper'>
       <Header
-        title= {`Welcome ${user?.name?? 'Guest'} 👋`}
+        title= {`Welcome ${user?.name ?? 'Guest'} 👋`}
         description='Track activity, trends and popular destinations in real time'
       />
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
 
+          <StatsCard
+            headerTitle='Total Users'
+            total= {totalUsers}
+            currentMonthCount= {usersJoined.currentMonth}
+            lastMonthCount= {usersJoined.lastMonth}
+          />
+          <StatsCard
+            headerTitle='Total Trips'
+            total= {totalTrips}
+            currentMonthCount= {tripsCreated.currentMonth}
+            lastMonthCount= {tripsCreated.lastMonth}
+          />
+          <StatsCard
+            headerTitle='Active Users Today'
+            total= {userRole.total}
+            currentMonthCount= {userRole.currentMonth}
+            lastMonthCount= {userRole.lastMonth}
+          />
+        </div>
+      </section>
+      <section className="container">
+        <h1 className='text-xl font-semibold text-dark-100'>Created Trips</h1>
+
+        <div className="trip-grid">
+          {allTrips.slice(0,4).map(({id, name, imageUrls, itinerary, tags, estimatedPrice}) => (
+            <TripCard
+              key={id}
+              id={id.toString}
+              name={name}
+              imageUrl={imageUrls[0]}
+              location={itinerary?.[0]?.location ?? ''}
+              tags={tags}
+              price={estimatedPrice}
+            />
+          ))}
+        </div>
+      </section>
     </main>
   )
 }
